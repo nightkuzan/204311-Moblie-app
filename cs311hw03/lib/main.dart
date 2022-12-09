aba581a2ded3b2af7157f3537fff02500135e1f3
nightkuzan <78246688+nightkuzan@users.noreply.github.com>
2022-12-09 13:30:35 +0700

    hw3

---
+++ cs311hw03/lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment #3 Hello User',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Assignment #3 Hello User'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // var for string or null
+  String? _username;
+  bool _isLogin = false;
+  Future<void> _loginUser() async {
+    setState(() {
+      _isLogin = false;
+    });
+    _username = await getUserInfo();
+    setState(() {
+      _isLogin = true;
+    });
+  }
+
+  String greetText() {
+    if (_isLogin == false) {
+      return 'Logging in...';
+    } else if (_username == null) {
+      return 'Login fails';
+    } else {
+      return 'Hello $_username';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              greetText(),
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loginUser,
+        tooltip: 'login',
+        child: const Icon(Icons.login),
+      ),
+    );
+  }
+}
+
+/*
+Provided Async function getUserInfo
+ */
+int index = 0;
+final usernameList = ['Peter', 'Jacob', 'Steve', null];
+Future<String?> getUserInfo() async {
+  index = index % usernameList.length;
+  return Future.delayed(
+      const Duration(seconds: 3), () => usernameList[index++]);
+}

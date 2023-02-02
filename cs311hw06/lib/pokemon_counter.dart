d840e6966a200f8d8247e8007b37cf9e39cb0501
nightkuzan <aekkarit12@gmail.com>
2023-02-02 10:14:55 +0700

    fix hw6

--- cs311hw06/lib/pokemon_counter.dart
+++ cs311hw06/lib/pokemon_counter.dart
@@ -1,3 +1,5 @@
+import 'package:cs311hw06/pokemon_them.dart';
+
 import 'pokemon_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,11 +15,12 @@ class PokemonCounter extends StatefulWidget {
 class _PokemonCounterState extends State<PokemonCounter> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<PokemonService>(
-      builder: (_, pokemonService, __) {
+    return Consumer2<PokemonService,PokemonThem>(
+      builder: (_, pokemonService,pokemonTheme, __) {
         return Scaffold(
           appBar: AppBar(
             title: const Text('Pokemon Count'),
+            backgroundColor:pokemonTheme.currentTheme, 
           ),
           body: const Center(
             child: Count(),
